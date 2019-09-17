f41763cedc201f792667e6cdaa5dc62a72c69ce4
Wang Jiang <sillyemperor@163.com>
2019-09-17 09:55:48 +0800

    simplify code

--- python/py3study/pygame-tutorials/draw-sprite.py
+++ python/py3study/pygame-tutorials/draw-sprite.py
@@ -9,14 +9,15 @@ pygame.init()
 
 screen = pygame.display.set_mode((600, 480))
 
-rect = pygame.Rect(10, 10, 214, 43)
-
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
     screen.fill((255, 255, 255))
-    pygame.draw.rect(screen, [255, 0, 0], rect)
+    #left, top, width, height
+    pygame.draw.rect(screen, [255, 0, 0], (10, 10, 214, 43))
+    #center, radius
     pygame.draw.circle(screen, [0, 255, 0], (300, 300), 100)
+    #[point,point,...]
     pygame.draw.polygon(screen, [0, 0, 255], [(20, 30), (55, 78), (49, 120)])
     pygame.display.flip()
